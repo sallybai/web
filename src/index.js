389f633cb52bc5a4794e0eb6e5f41ba326e64713
SallyBai <475575788@qq.com>
2018-03-23 16:24:31 +0000

    final prototype

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import 'bootstrap'
 
 import 'bootstrap/dist/css/bootstrap.css'
-
+$('.page2').hide();
 let btn = $('button')
+$('#career').click(function(){
+  $('.page2').show();
+  $('.page1').hide();
+})
+$('#index').click(function(){
+  $('.page1').show();
+  $('.page2').hide();
+})
 
 console.log(btn)
 $('.carousel').carousel({
